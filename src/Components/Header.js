6e385ec21df953d4f9c1e359b526ca34cd63445c
Bishesh <bishesh.sunam@gmail.com>
2021-01-05 22:14:50 +1300

    styled header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import '../Components/Header.css';
+import {Avatar} from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 function header() {
     return (
         <div className='header'>
-            <h1>Hello from header</h1>
+            <div className="header__left">
+                <Avatar className='header__avatar' alt='avatar' />
+                <AccessTimeIcon />
+                {/* Time icon */}
+            </div>
+
+            <div className="header__search">
+                <SearchIcon />
+                <input type="text" className='header__search' placeholder="Search anything"/>
+            </div>
+
+            <div className="header__right">
+              <HelpOutlineIcon />
+            </div>
         </div>
     )
 }
