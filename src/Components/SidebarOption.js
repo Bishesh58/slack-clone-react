f796fd880061603858c70b45b904185dceee7d39
Bishesh <bishesh.sunam@gmail.com>
2021-01-07 21:47:41 +1300

    added add-channel functionality

--- src/Components/SidebarOption.js
+++ src/Components/SidebarOption.js
@@ -1,9 +1,32 @@
- import React from 'react';
+ import { InfoOutlined } from '@material-ui/icons';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
  import './SidebarOption.css';
+ import db from '../firebase';
  
- function SidebarOption({ Icon, title }) {
+ function SidebarOption({ Icon, title, addChannelOption, id}) {
+
+    const history = useHistory();
+
+    const selectChannel = () =>{
+        //if id is from database
+        if(id){
+            history.push(`/room/${id}`);
+        } else{
+            history.push(title);
+        }
+    }
+
+    const addChannel =()=> {
+        const channelName = prompt('Please enter the channel name')
+        if(channelName){
+            db.collection('rooms').add({
+               name: channelName
+            })
+        }
+    }
      return (
-         <div className='sidebarOption'>
+         <div className='sidebarOption' onClick={addChannelOption ? addChannel : selectChannel}>
              {Icon && <Icon className='sidebarOption__icon'/>}
              {Icon? (
                 <h3>{title}</h3>
