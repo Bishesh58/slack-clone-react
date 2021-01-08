8957a8f26c7621e23e44e46db5b1e78387afc945
Bishesh <bishesh.sunam@gmail.com>
2021-01-08 23:55:35 +1300

    added login page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Chat from './Components/Chat';
+import { useState } from 'react';
+import Login from './Components/Login';
 
 
 function App() {
+
+  //initially there will be no user so that user can login, then the state will have user
+  const [user, setUser] = useState(null);
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          //you cannot have two child to return so wrap header & div with one parent fragment(<>)
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Switch>
+              <Route path="/room/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+              </Route>
+            </Switch>
+          </div>
+        </>
+        )}
+        
       </Router>
     </div>
   );
